acfba3a0fdd2fbbbf6d8d54ea00604cac7888cba
Shakame <lucas.baujard1@gmail.com>
2023-12-07 21:55:26 +0100

    Ajout couleurs différentes matrice

--- script.js
+++ script.js
@@ -21,26 +21,80 @@ function drawShape(ctx, x, y, r, sides, rot) {
     ctx.resetTransform();
   }
 
-function renderCanvas() {
+  
+  function hslToRgb(h, s, l) {
+    let r, g, b;
+  
+    if (s === 0) {
+      r = g = b = l; // achromatic
+    } else {
+      const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      const p = 2 * l - q;
+      r = hueToRgb(p, q, h + 1/3);
+      g = hueToRgb(p, q, h);
+      b = hueToRgb(p, q, h - 1/3);
+      console.log("r: ", r.toString(16))
+      console.log("g: ", g)
+      console.log("b: ", b)
+    }
+  
+    var color = "#"
+    color += (Math.round(r * 255)).toString(16)
+    color += (Math.round(g * 255)).toString(16)
+    color += (Math.round(b * 255)).toString(16)
+    console.log(color)
+    return color;
+  }
+
+  function hueToRgb(p, q, t) {
+    if (t < 0) t += 1;
+    if (t > 1) t -= 1;
+    if (t < 1/6) return p + (q - p) * 6 * t;
+    if (t < 1/2) return q;
+    if (t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+    return p;
+  }
+
+  function hsvToRgb(h, s, v) {
+    var r, g, b;
+  
+    var i = Math.floor(h * 6);
+    var f = h * 6 - i;
+    var p = v * (1 - s);
+    var q = v * (1 - f * s);
+    var t = v * (1 - (1 - f) * s);
+  
+    switch (i % 6) {
+      case 0: r = v, g = t, b = p; break;
+      case 1: r = q, g = v, b = p; break;
+      case 2: r = p, g = v, b = t; break;
+      case 3: r = p, g = q, b = v; break;
+      case 4: r = t, g = p, b = v; break;
+      case 5: r = v, g = p, b = q; break;
+    }
+    var color = "rgb("
+    color += r.toString() +", "
+    color += g.toString() +", "
+    color += b.toString() + ")"
+    console.log(color)
+    return color;
+  }
+    
+  function renderCanvas() {
     var canvas = document.getElementById("canvas")
     const ctx = canvas.getContext("2d")
     canvas.width = 1000;
     canvas.height = 1000;
-    /*for (let i = 0 ; i < 3 ; i++) {
-        for (let j = 0 ; j < 3 ; j++) {
-            drawShape(ctx, 50+100*i, 50+100*j, 50, 4, 45)
-        }
-    }*/
-    ctx.fillStyle = hsl(50, 80%, 40%)
-    ctx.fillRect(0, 0, 100, 100);
+
+    let colors = ["#11ff22", "#4455ee"]
     for (let i = 0 ; i < 3 ; i++) {
         for (let j = 0 ; j < 3 ; j++) {
-            ctx.fillRect(0 + 105 * i, 0 + 105 * j, 100, 100);
+          //console.log("HSL("+(120 + i*30 + j*20).toString()+", 45%, 45%)")
+          
+          ctx.fillStyle = "HSL("+(0 + (i+3*j)*40).toString()+", 45%, 45%)"//hsvToRgb(10 + i*5 + j, 90, 90);
+          ctx.fillRect(0 + 105 * i, 0 + 105 * j, 100, 100);
         }
     }
-    
-    const cx = 200;
-    const cy = 100;
 }
 
 
